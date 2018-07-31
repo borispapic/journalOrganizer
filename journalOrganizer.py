e65d8f14a1a3cf442b4f4402be38e858f6fed1a8
ChuckBoris96 <33844801+ChuckBoris96@users.noreply.github.com>
2018-07-31 16:17:43 +0200

    Add files via upload

---
+++ journalOrganizer.py
@@ -0,0 +1,11 @@
+import os, shutil, datetime
+
+now = datetime.datetime.now()
+monthDict={1:'Januar', 2:'Februar', 3:'Mart', 4:'April', 5:'Maj', 6:'Jun', 7:'Jul', 8:'Avgust', 9:'Septembar', 10:'Oktobar', 11:'Novembar', 12:'Decembar'}
+print(now.month)
+print(monthDict[now.month])
+
+for filename in os.listdir(r'C:\Users\Boris\Desktop'):
+    if filename == 'dnevnik.txt':
+        newName = str(now.day) + '-' + str(now.month) + '.txt'
+        shutil.move('C:\\Users\\Boris\\Desktop\\{0}'.format(filename),'C:\\Users\\Boris\\Desktop\\Journal\\{0}\\{1}\\{2}'.format(now.year,monthDict[now.month],newName))
