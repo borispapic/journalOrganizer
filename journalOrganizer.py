2e813da750d620e6d9de73110cb066f0dd63e0ab
BorkoP <33844801+borispapic@users.noreply.github.com>
2021-04-25 22:22:31 +0200

    Add files via upload

--- journalOrganizer.py
+++ journalOrganizer.py
@@ -1,17 +1,16 @@
 import os, shutil, datetime
 
+JOURNAL_PATH= "D:\\PRIV\\Journal"
 now = datetime.datetime.now()
 monthDict={1:'Januar', 2:'Februar', 3:'Mart', 4:'April', 5:'Maj', 6:'Jun', 7:'Jul', 8:'Avgust', 9:'Septembar', 10:'Oktobar', 11:'Novembar', 12:'Decembar'}
 #print(now.month)
 #print(monthDict[now.month])
-if now.day == 1:
-    os.makedirs('C:\\Users\\Boris\\Desktop\\Journal\\{0}\\{1}'.format(now.year,monthDict[now.month]))
-    if now.day == 1 and now.month == 1:
-         os.makedirs('C:\\Users\\Boris\\Desktop\\Journal\\{0}'.format(now.year))
+if os.path.exists(f"{JOURNAL_PATH}\\{now.year}\\{now.month}") == False:
+    #os.makedirs('C:\\Users\\Boris\\Desktop\\Journal\\{0}\\{1}'.format(now.year,monthDict[now.month]))
+    os.makedirs(f"{JOURNAL_PATH}\\{now.year}\\{monthDict[now.month]}")
                 
-
-for filename in os.listdir(r'C:\Users\Boris\Desktop'):
+desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+for filename in os.listdir(desktop):
     if filename == 'dnevnik.txt':
         newName = str(now.day) + '-' + str(now.month) + '.txt'
-        shutil.move('C:\\Users\\Boris\\Desktop\\{0}'.format(filename),'C:\\Users\\Boris\\Desktop\\Journal\\{0}\\{1}\\{2}'.format(now.year,monthDict[now.month],newName))
-
+        shutil.move(f"{desktop}\\{filename}",f"{JOURNAL_PATH}\\{now.year}\\{monthDict[now.month]}\\{newName}")
